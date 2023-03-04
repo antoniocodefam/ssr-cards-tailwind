38127a7f1ae62edb6b5e8c6f3d1b33f469c4201c
Toni <antonio.codefam@gmail.com>
2023-03-04 13:42:36 +0100

    Update mobile PostCard image styling

--- ssr-cards-tailwind/src/components/PostCard/index.tsx
+++ ssr-cards-tailwind/src/components/PostCard/index.tsx
@@ -11,14 +11,16 @@ export default function PostCard({ post }: PostCardProps) {
     <div
       className={`${styles.card} flex flex-col w-80 shadow-md h-full bg-slate-50 rounded-md`}
     >
-      <Image
-        src={post.photo.url}
-        alt={post.photo.title}
-        title={post.photo.title}
-        width={320}
-        height={300}
-        className="rounded-t-md"
-      />
+      <div className={styles.image}>
+        <Image
+          src={post.photo.url}
+          alt={post.photo.title}
+          title={post.photo.title}
+          width={320}
+          height={300}
+          className="rounded-t-md"
+        />
+      </div>
       <div className="p-3">
         <h3 className="text-lg mb-2 uppercase">{post.title}</h3>
         <p className="text-sm text-slate-700">{post.body}</p>
